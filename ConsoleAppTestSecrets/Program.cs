193c767ef6800dab175ba9f068f95b29ec5928bb
Lukas Frystak <lukas.frystak@sonarsource.com>
2024-02-29 14:35:14 +0100

    Project setup

---
+++ ConsoleAppTestSecrets/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace ConsoleAppTestSecrets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            IConfiguration config = builder.Build();
+
+            var batchSize = config["AutoNumberOptions: BatchSize"];
+
+            Console.WriteLine($"Batch Size {batchSize}");
+        }
+    }
+}
